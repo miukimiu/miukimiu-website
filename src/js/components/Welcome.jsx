37f3268b7d39034355a4411fec7bd583c737b44a
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-04-25 23:29:46 +0100

    Header structure and styles

--- src/js/components/Welcome.jsx
+++ src/js/components/Welcome.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import airBalloon from '../../images/airBalloon.svg';
+import logo from '../../images/logo.svg';
 
-const Welcome = () => {
-
-  return (
-    <section>
-      <h1>ops</h1>
-    </section>
-  );
-};
+const Welcome = () => (
+  <section className="welcome">
+    <img className="welcome__air-balloon" src={airBalloon} alt="" />
+    <img className="welcome__logo" src={logo} alt="" />
+  </section>
+);
 
 export default Welcome;
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -44,7 +44,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
         use: [
           'file-loader'
         ]
