8d3b50721df83a801208c4686473530e94d8bb9d
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-05-06 21:58:05 +0100

    New section

--- src/js/components/Work.jsx
+++ src/js/components/Work.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { KawaiiIceCream } from 'react-kawaii';
 
 const Work = () => (
   <section className="section section--dark work">
-    <h2>Work</h2>
+    <svg
+      className="work__separator"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="none"
+    >
+        <path d="M0 100 C 20 0 50 0 100 100 Z"></path>
+    </svg>
+    <article className="section__article">
+      <h2>I love my work</h2>
+      <p>On my free time I like to </p>
+    </article>
   </section>
 );
 
