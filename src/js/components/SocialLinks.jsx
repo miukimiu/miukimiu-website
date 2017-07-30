11e2da5f1883ec551a9e3ae794abe32358cc5572
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-07-30 23:09:55 +0100

    Social icons

--- src/js/components/SocialLinks.jsx
+++ src/js/components/SocialLinks.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import codepenImg from '../../images/social-icons/codepen.svg';
+import dribbbleImg from '../../images/social-icons/dribbble.svg';
+import githubImg from '../../images/social-icons/github.svg';
+import linkedinImg from '../../images/social-icons/linkedin.svg';
+import mediumImg from '../../images/social-icons/medium.svg';
+import twitterImg from '../../images/social-icons/twitter.svg';
 
 const SocialLinks = () => (
   <section className="social-links">
-    <a href="https://github.com/miukimiu">
-      <i className="fi flaticon-logo" />
-    </a>
-    <a href="https://twitter.com/miuki_miu">
-      <i className="fi flaticon-twitter-logo" />
+
+    <a href="https://codepen.io/miukimiu">
+      <img alt="" src={codepenImg} />
     </a>
     <a href="https://dribbble.com/miukimiu">
-      <i className="fi flaticon-dribbble-logo" />
+      <img alt="" src={dribbbleImg} />
     </a>
-    <a href="https://www.instagram.com/miukimiu/">
-      <i className="fi flaticon-instagram-logo" />
+    <a href="https://github.com/miukimiu">
+      <img alt="" src={githubImg} />
+    </a>
+    <a href="https://www.linkedin.com/in/elizabetoliveira/">
+      <img alt="" src={linkedinImg} />
+    </a>
+    <a href="https://medium.com/@miuki_miu">
+      <img alt="" src={mediumImg} />
+    </a>
+    <a href="https://twitter.com/miuki_miu">
+      <img alt="" src={twitterImg} />
     </a>
   </section>
 );
