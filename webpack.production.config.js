2d142aa6291db74672e002f1cee90f3482fcd09b
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-09-22 01:55:19 +0100

    Webpack svg fix

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       // { test: /\.scss$/, loader: ExtractTextPlugin.extract('style','css!sass') },
       {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
         use: [
           'file-loader?name=[path][name].[ext]&outputPath=/&publicPath=/'
         ]
